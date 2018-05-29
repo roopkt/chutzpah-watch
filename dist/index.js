39231604eae85537404b7601bb4476eee43261aa
roopkt <roopkt@gmail.com>
2018-05-29 11:49:16 -0400

    updated the npm version

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const yargs = require("yargs");
 const fs_1 = require("fs");
 const watch_testfiles_1 = require("./watch-testfiles");
-yargs
-    .config('chutzpah', configPath => JSON.parse(fs_1.readFileSync(configPath, 'utf-8')))
+const y = yargs
+    .config('config', (configPath) => JSON.parse(fs_1.readFileSync(configPath, 'utf-8')))
     .command({
     command: 'watch',
     handler: (argv) => {
         watch_testfiles_1.run(argv);
-    }
+    },
 })
     .argv;
 //# sourceMappingURL=index.js.map
