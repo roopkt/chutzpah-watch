7d122bd13bc2d7a78550dfe13ecb16b0a6c4d7fe
roopkt <roopkt@gmail.com>
2018-05-30 12:41:41 -0400

    updating command help texts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,26 @@ import { readFileSync } from 'fs';
 import { run } from './watch-testfiles';
 
 const y = yargs
-    .config('config', (configPath: string) => JSON.parse(readFileSync(configPath, 'utf-8')))
-    .command({
-        command: 'watch',
-        handler: (argv: any) => {
-            run(argv);
-        },
+  .option(
+    'config',
+    {
+      alias: 'c',
+      describe: 'chutzpah-watch cli options',
+      demandOption: true,
     })
-    .argv;
+  .demandOption(
+    ['config'],
+    'Please provide config argument to work with this tool')
+  .config(
+    'config',
+    (configPath: string) => JSON.parse(readFileSync(configPath, 'utf-8')))
+  .command({
+    command: 'chutzpah <watch>',
+    describe: 'run tests in watch mode',
+    handler: (argv: any) => {
+      run(argv);
+    },
+  })
+  .example('chutzpah watch --config chuzpah-wath.json', 'running chutzpah-watch and reading cli options from given config file')
+  .help()
+  .argv;
